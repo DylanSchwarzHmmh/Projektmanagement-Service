ca9111f16e3516a2f6fd1ed7069ef7dbcfe5f984
Justus Krebsfänger <justus.krebsfaenger@gmail.com>
2023-11-08 11:32:50 +0100

    Added QualificationDto's to work with Qualifications from szut.de

--- src/main/java/de/szut/lf8_project/employee/GetQualificationDto.java
+++ src/main/java/de/szut/lf8_project/employee/GetQualificationDto.java
@@ -2,12 +2,15 @@ package de.szut.lf8_project.employee;
 
 import lombok.Getter;
 import lombok.Setter;
+import reactor.util.annotation.Nullable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Getter
 @Setter
 public class GetQualificationDto {
+    @Nullable
     private Long id;
     private String skill;
 }
---
+++ src/main/java/de/szut/lf8_project/employee/GetQualificationsDto.java
@@ -0,0 +1,15 @@
+package de.szut.lf8_project.employee;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class GetQualificationsDto {
+    private Long id;
+    private String lastName;
+    private String firstName;
+    private Set<GetQualificationDto> skillSet;
+}
